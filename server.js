f88417ecc5d1bdfceece289da81cf5862d18e081
Raebon <kuceradominik123@gmail.com>
2021-11-02 07:14:15 +0100

    Initial Author routes

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 
@@ -21,11 +23,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB!"))
   .catch((error) => console.error("Could not connect to MongoDB... ", error));
 
-/* mongoose.connect(process.env.DATABASE_URL, { useNewUrlParse: true });
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Připojeno k Mongoose")); */
-
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
